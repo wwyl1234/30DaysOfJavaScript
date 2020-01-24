71afcf6b19ad80e28d679e579d5701d89d01a190
Winnie Lam <winnielam@Winnies-MacBook-Air.local>
2020-01-23 19:50:02 -0500

    Worked on Exercises 22 on project 1 and project 2.

--- 22_Day/22_day_starters/project_1/scripts/main.js
+++ 22_Day/22_day_starters/project_1/scripts/main.js
@@ -1,2 +1,64 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// Exercise 22
+// Level 1
+// Create a div container on HTML document and create 100 to 100 numbers
+// dynamically and append to the container div.
+//
+//    Even numbers background is green
+//    Odd numbers background is yellow
+//    Prime numbers background is red
+
+// Create the container div
+let container = document.createElement('div');
+container.className = 'container';
+container.style.justifyContent = 'center';
+container.style.display = 'flex';
+container.style.flexFlow = 'row wrap';
+container.style.maxWidth = '700px';
+document.body.appendChild(container);
+
+// Returns true if number is prime. Otherwise false.
+function isPrime(num){
+  if (num < 2){
+    return false;
+  }
+
+  if (num == 2 || num == 3){
+    return true;
+
+  }
+  // go through all numbers from 2 to sqrt of number
+  let stopNum = Math.floor(Math.sqrt(num));
+  for (let i = 2; i <= stopNum; i++){
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+// make the boxes from 0 to 101
+for (let i = 0; i < 102; i++) {
+    let number = document.createElement('div');
+    number.style.width = '100px';
+    number.style.height = '40px';
+    number.style.fontSize = '30px';
+    number.style.color = 'white';
+    number.style.textAlign = 'center';
+
+    number.style.margin = '0 10px 10px 0';
+    number["textContent"] = `${i}`;
+    if (i % 2 == 0){
+      // even then background should be green
+      number.style.backgroundColor = 'green';
+    } else {
+      // odd then background should be yellow
+      number.style.backgroundColor = 'yellow';
+    }
+
+    // if prime then background should be red
+    if (isPrime(i)){
+      number.style.backgroundColor = 'red';
+    }
+    container.appendChild(number);
+}
--- 22_Day/22_day_starters/project_2/scripts/main.js
+++ 22_Day/22_day_starters/project_2/scripts/main.js
@@ -1,1 +1,27 @@
 console.log(countries)
+
+// Modify the class countries-wrapper
+let wrapper = document.getElementsByClassName('countries-wrapper')[0];
+// wrapper.style.justifyContent = 'center';
+wrapper.style.display = 'flex';
+wrapper.style.flexFlow = 'row wrap';
+wrapper.style.maxWidth = '700px';
+//wrapper.style.alignItems = 'center';
+document.body.appendChild(wrapper);
+
+// Make a div element for each country
+for (const country of countries){
+  let box = document.createElement('div');
+  box.style.width = '100px';
+  box.style.height = '100px';
+  box.style.fontSize = '10px';
+  box.style.color = 'black';
+  box.style.border = '0.5px solid grey';
+  box.style.borderRadius = '5%';
+  box.style.display = 'flex';
+  box.style.margin = '1px 1px 1px 1px';
+  box.style.justifyContent = 'center';
+  box.style.alignItems = 'center';
+  box["textContent"] = `${country}`;
+  wrapper.appendChild(box);
+}
